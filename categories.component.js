8fb92a6ca063621498e62283d4902b596fa737ec
yazminjackson <145791758+yazminjackson@users.noreply.github.com>
2023-10-06 13:15:10 -0500

    Create categories.component.js

---
+++ categories.component.js
@@ -0,0 +1,15 @@
+(function () {
+	
+	angular.module('MenuApp').component('categories',{
+		//template: '<div>testLength={{$ctrl.categorieList}}In categories component</div>',
+		templateUrl: 'categories.component.template.html',
+		bindings:{
+			categorieList:'<',
+		}
+		
+
+
+	});
+
+
+})();
